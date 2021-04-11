f59afea36363161f9128fdd4a1241f85142de54b
bokyungseo <maybutter756@gmail.com>
2021-04-11 16:56:39 +0900

    위젯 갱신 관련 코드 수정

--- DdayWidget/DdayWidget.swift
+++ DdayWidget/DdayWidget.swift
@@ -22,9 +22,12 @@ struct Provider: IntentTimelineProvider {
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [DdayEntry] = []
         let currentDate = Date()
-        let entry = DdayEntry(date: currentDate, configuration: configuration)
-        entries.append(entry)
-        let timeline = Timeline(entries: entries, policy: .never)
+        for offset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .second, value: offset, to: currentDate)!
+            let entry = DdayEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+        let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
 }
